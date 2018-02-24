de2c42e91ddeff2af7b271548779e6ed833f3d0c
Leonardo Rico <leonardo.ricog@gmail.com>
2018-02-24 13:54:22 -0500

    Version 1.0.0

---
+++ src/components/Example.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ExampleComponent extends Component {
+  render() {
+    return (
+      <div>This is so Example!</div>
+    );
+  }
+}
+export default ExampleComponent;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ExampleComponent from './components/Example';
+
+export {
+  ExampleComponent as Example
+};
+
+/**
+ * Usage:
+ * import { Example } from 'react-npm-component'
+ * import * as ReactNpmComponent from 'react-npm-component'
+ */
